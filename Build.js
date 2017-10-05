86d30c84ab826496fd1f4c5c43ad493184eb8b7d
YuseiIto <yuseikun@outlook.jp>
2017-10-05 20:03:03 +0900

    Initial Commit

---
+++ Build.js
@@ -0,0 +1,136 @@
+﻿function OnfileSelect(files) {
+    var reader = new FileReader();
+    reader.onload = function(event) {
+
+        var Img = new Image();
+        Img.onload = function() {
+
+            var canvas = document.getElementById('InfCvs');
+            canvas.width = Img.naturalWidth;
+            canvas.height = Img.naturalHeight;
+            var ctx = canvas.getContext('2d');
+            ctx.globalAlpha = 1.0;
+            ctx.drawImage(Img, 0, 0);
+
+        }
+        Img.src = event.target.result;
+
+    }
+    reader.readAsDataURL(files[0]);
+    document.getElementById('filename').innerHTML = "選択済み:" + files[0].name;
+}
+
+//Encode process
+function Go() {
+
+    var num = String(editAreaLoader.getValue("editor_js")).length;
+    var retext = "";
+    if (num > 0) {
+        for (var i = 0; i < num; i++) {
+            //Get a character and to be code
+            var code = String(editAreaLoader.getValue("editor_js")).charCodeAt(i);
+            //connect each character's code
+            if (code > -1) {
+                retext += "&#" + code + ";";
+            }
+        }
+
+    }
+
+
+    var obj = new InfinityData(); //Class defined at InfinityData.js
+    obj.Text = "[#]" + retext + "[;]";
+
+    var str = obj.Encode();
+
+    //canvas initialize process
+    document.getElementById('Cvs_Ara').innerHTML = '<canvas id="InfCvs" style="display:none;"></canvas>';
+
+    //image visible process
+    document.getElementById('img').style = "border:solid 3px #000000;padding:4px;";
+    document.getElementById('img').src = str;
+    document.getElementById('img').style.width = '50%';
+
+    alert("お待たせしました。　エンコードが完了しました。");
+
+}
+
+//プレビューについての処理
+function Preview() {
+    console.log("Reload called");
+
+    var str = editAreaLoader.getValue("editor_js");
+    console.log('PRM=' + str);
+    var iframe = document.getElementById('preview');
+    if (iframe.contentDocument) {
+        iframe.contentDocument.open();
+        iframe.contentDocument.write(str);
+        iframe.contentDocument.close();
+    }
+    document.getElementById("LoadFile").innerHTML = "";
+    document.getElementById("LoadFile").innerHTML = '<input type="file"  OnChange="Go(this.files)">';
+}
+
+function Read_Img(file) {
+    var reader = new FileReader();
+
+
+    //ファイルの読込が終了した時の処理
+    reader.onload = function() {
+            var obj = new InfinityData();
+            obj.DataUrl = reader.result;
+            console.log(obj.DataUrl);
+            console.log("Decode Start");
+            obj.Decode(false);
+
+        }
+        //dataURL形式でファイルを読み込む
+    reader.readAsDataURL(file[0]);
+}
+
+
+function Read_Html(file) {
+    var reader = new FileReader();
+
+
+    //ファイルの読込が終了した時の処理
+    reader.onload = function() {
+            var str = reader.result;
+            editAreaLoader.setValue("editor_js", str);
+            var iframe = document.getElementById('preview');
+            if (iframe.contentDocument) {
+                iframe.contentDocument.open();
+                iframe.contentDocument.write(str);
+                iframe.contentDocument.close();
+            }
+            document.getElementById("LoadFile").innerHTML = "";
+            document.getElementById("LoadFile").innerHTML = '<input type="file"  OnChange="Go(this.files)">';
+        }
+        //dataURL形式でファイルを読み込む
+    reader.readAsText(file[0]);
+}
+
+
+function Read(files) {
+
+
+    var file = files[0].name;
+    var f = file.split('.');
+    var type = f[f.length - 1].toLowerCase();
+
+    if (type == "html" || type == "txt" || type == "htm") {
+        //When HTML
+        Read_Html(files);
+    } else if (type == "svg" || type == "png" || type == "jpeg" || type == "jpg" || type == "bmp" || type == "gif") {
+        //WhenImg
+        Read_Img(files);
+    }
+
+    document.getElementById('preview').style.height = "370px";
+    document.getElementById('inputarea').innerHTML = '<div class="preview" ">\
+	<div class="input-file-view">\
+		<img src="pics/OpenFile.svg" style="height:80px;margin-right:auto; margin-left:auto;" id="ico" class="button" >\
+	</div>\
+	</div>\
+	<input class="input-file-core" type="file" Onchange="OnfileSelect(this.files);"accept="Image/*,.htm,.html,.txt,HTML,HTM">';
+}
---
+++ README.md
@@ -0,0 +1,95 @@
+﻿# Easy Apps Exchange(2.0.0)
+
+## Overview  
+Easy Apps Excahnge writes your HTML app to any image file.
+
+## Description  
+Easy Apps Excahnge writes your HTML app to any image file.
+Doing it makes your HTML app available offline and SNS share . 
+
+
+***DEMO:***
+You can see this at [here](https://yuseiito.github.io/EasyAppsExchange.html).
+
+
+## Features  
+* Encode HTML to PNG.
+* View the PNG to HTML
+
+
+## Requirement
+* This needs HTML5 , JavaScript ,CSS3 Web Browser.
+**Including:** Infinity Data.js Custom version.
+
+## Usage
+### To write HTML to image file
+You use [Builder] when you write HTML to image file.
+1. Open your HTML app from [Select file]  
+(If you don't have HTML app , You can skip this step.)
+ 
+2. Write or Edit your HTML at code high light editor area.
+
+3. Select image at under the editor.  
+(Easy Apps Exchange writes HTML to the image that you selected here.)
+
+**Note:**
+* Click or Tap  [Reload and Preview]   
+to reload preview your app when you are editing your HTML app.
+
+### To see HTML app from image.
+You use [Player] when you see HTML app.  
+Just select Image file to see HTML app.
+
+
+## Installation
+You don't need to install this app.
+But if you want to use this app at offline , please clone  this repository and Open HTML : EAX_Build.html or EAX_Play.html or About.html .
+
+
+## Bugs,To do later.
+* Now, bugs were not found.
+* I'll make frequency modulation system.
+When the system is added, Difference between original image , encoded image becomes to be a little.
+
+## FAQ
+If you get any question about this, please ask me.
+
+
+## Contribution
+Please contribution!
+Such as...
+* Wrire README.md or Error message in each language.
+* Report or fix bug
+* Features request
+
+## Authors / Copying / Thanks
+This software includes
+[EditArea - © Christophe Dolivet 2007-2010](http://www.cdolivet.com/editarea/)
+ that is distributed in the Apache License 2.0.
+ Also I got some icons from [icooon-mono](http://icooon-mono.com/)
+ .
+
+## Author
+**Yusei Ito**
+* Twitter[@ItyuJ](https://twitter.com/ItyuJ/)
+* Github [Yusei Ito](https://github.com/yuseiito/)
+
+## Awarded
+### 「U-22 プログラミング　コンテスト2017」
+#### (U-22 programming contest 2017)
+
+* 経済産業省商務情報政策局長賞  
+（The award Director-General, Commerce and Information Policy Bureau,METI.)
+
+* Useful(日本事務器)賞  
+("Useful award" by Nippon Jimuki Co.,Ltd.)
+
+### 「PCNこどもプロコン 2017 夏」
+#### (PCN children programming contest of summer in 2017)
+* さくらインターネット賞
+(The award SAKURA Internet)
+
+## Licence 
+Released under the MIT license
+Copyright (c) Yusei Ito 2017. - Easy Apps Exchange
+
